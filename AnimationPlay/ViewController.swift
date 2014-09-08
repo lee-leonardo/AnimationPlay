ed7ddba21c7a3adb585ceea09448542c3ceb28b1
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-07 23:03:27 -0700

    Did research. From what I know now: 1. It would be actually easier to develop an algorithm that gives a list of points to an animation for a pair of foot prints to walk to. I am going to do a bit of playing with the UIView for a bit and then CAAnimation to figure out the best tooling for it.

--- AnimationPlay/ViewController.swift
+++ AnimationPlay/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
@@ -22,6 +23,11 @@ class ViewController: UIViewController {
         redBall.layer.cornerRadius = 50
         self.view.addSubview(redBall)
         
+        var oval = UIView(frame: CGRectMake(80, 200, 50, 100))
+        oval.backgroundColor = UIColor.blueColor()
+        oval.layer.cornerRadius = 15
+        self.view.addSubview(oval)
+        
         UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
             () -> Void in
             redBall.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(2, 2), CGAffineTransformMakeTranslation(75, 0))
@@ -29,6 +35,33 @@ class ViewController: UIViewController {
             redBall.backgroundColor = UIColor.greenColor()
             
             }, completion: nil)
+        
+        
+        //CAAnimations are attached to either a CALayer object or a layer property of a UIView.
+        var layer = CALayer() //This is how you can make a CALayer.
+        //From the docs
+        //Layer objects must have a content property with the bitmap.
+        //
+        
+        
+        var translateX = 15
+        var translateY = 15
+        
+        UIView.animateWithDuration(1, delay: 0.2, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            oval.transform = CGAffineTransformRotate(oval.layer.affineTransform(), CGFloat(M_2_PI))
+
+        }) { (bool) -> Void in
+            UIView.animateWithDuration(2, animations: { () -> Void in
+                oval.transform = CGAffineTransformMakeTranslation(oval.center.x + translateX, oval.center.y + translateY)
+            })
+        }
+        
+        
+        //var viewLayer = UIView(frame: CGRectMake(50, 50, 100, 100))
+        //viewLayer.layer
+        
+        
+        //JNWSpringAnimation may be something interesting to look into!
 
     }
 
